0491aa79ca41c6ea75dedd1609da504cd6838df2
Ben <benkurland1119@gmail.com>
2022-09-07 03:23:37 +0000

    t

--- src/mywork/system_equations_z3.py
+++ src/mywork/system_equations_z3.py
@@ -12,6 +12,6 @@ def system(a,b,c,d,e,f,g,h,i,j,k,l) :
         return s.model()
     return -1
     
-print(system(   3, 2,  -1, 1,
-                2, 2,   4,-2,
-               -1, .5, -1, 0))
+print(system(3, 2,-1, 1,
+             2, 2, 4,-2,
+            -1,.5,-1, 0))
